c4c6e606cd4ac7733acf06f779beaa34fa928314
charlesqu666 <94049036+charlesqu666@users.noreply.github.com>
2026-03-30 14:05:19 +0800

    Add files via upload
    updata

--- core/fasta_parser.hpp
+++ core/fasta_parser.hpp
@@ -1,22 +1,3 @@
-/*
- * SpacerScope - gRNA Design and Off-Target Effect Analysis Tool
- * Copyright (C) 2026 charlesqu666
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful for CRISPR
- * research and therapeutic applications, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE. See the GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- * 
- * Source code is available at: https://github.com/charlesqu666/SpacerScope
- */
 #pragma once
 #include <string>
 #include "types.hpp"
